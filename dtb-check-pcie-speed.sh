a3dec75c0bbc5c53e76fc15023977b89693705fe
Leif Sawyer <leif@akhepcat.com>
2023-07-03 08:37:45 -0800

    Add simple error-checking for devicetree support

--- dtb-check-pcie-speed.sh
+++ dtb-check-pcie-speed.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-dtc -I fs /sys/firmware/devicetree/base/ -qqq -H both -@ | grep max-link
+if [ -d /sys/firmware/devicetree ]
+then
+	dtc -I fs /sys/firmware/devicetree/base/ -qqq -H both -@ | grep max-link
+else
+	echo "Devicetree not supported on this system, this script might not be useful for you"
+fi
 
 echo "check PCI link speeds with this command:"
 echo "lspci -v -v 2>/dev/null | grep -E '^[0-9]|LnkCap'"
