9baf8e36551e811a2fdf3609d6788f90bd92f1f6
Leif Sawyer <leif@akhepcat.com>
2023-08-04 08:53:54 -0800

    update for armbianEnv; add support for orange5 (non-plus)

--- orangepi5plus-pwm-fanctl.sh
+++ orangepi5plus-pwm-fanctl.sh
@@ -14,9 +14,10 @@ fi
 
 # ------------
 
+envf=$(find /boot -maxdepth 1 -iname 'armbianEnv.txt' -o -iname 'orangepiEnv.txt')
 if [ -z "${PWMCHIP}" ]
 then
-	PWMS="$(grep -oE 'pwm\S+' /boot/orangepiEnv.txt)"	# "pwm14-m0"
+	PWMS="$(grep -oE 'pwm[0-9]+-m[0-9]+' ${envf:-/dev/null})"	# "pwm14-m0"
 	PWMA=($PWMS)
 	pc=${#PWMA[@]} 
 	if [ ${pc} -gt 1 ]
@@ -28,20 +29,20 @@ then
 	case ${PWMS%-*} in
 		pwm0)	IOADDR=fd8b0000;;
 		pwm1)	IOADDR=fd8b0010;;
+		pwm3)	IOADDR=fd8b0030;;
 		pwm10)  IOADDR=febe0020;;
 		pwm11)  IOADDR=febe0030;;
 		pwm12)  IOADDR=febf0000;;
 		pwm13)  IOADDR=febf0010;;
+		pwm13)  IOADDR=febf0010;;
 		pwm14)  IOADDR=febf0020;;
+		pwm15)  IOADDR=febf0030;;
 		*) echo "unknown PWM chip, can't determine IOADDR for disambiguation in /sys/class/pwm"; exit 1;;
 	esac
 
 	PWMCHIP=$(/bin/ls -l /sys/class/pwm/ | grep ${IOADDR} | grep -owE 'pwmchip[0-9]' | head -1)
 	PWMPORT=${PWMS#*-m}
 fi
-
-#PWMCHIP=pwmchip2		# PWM14_M0, which is pwm chip 2; there's only 1 pwm channel (pwm0) available on chip 2
-#PWMPORT=0
 
 FREQ=$(echo "scale = 5; print ( ( 1 / ${FREQ} ) * 1000000000 ); print \"\n\";"  | bc)
 FREQ=${FREQ/.*}
@@ -50,11 +51,20 @@ DUTYCYCLE=$(echo "scale = 5; print ( ${FREQ} - (${FREQ} * (${DUTYCYCLE}/100) ));
 DUTYCYCLE=${DUTYCYCLE/.*}
 
 # Make sure the pwm chip is available
-[[ ! -d /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT} ]] && \
-	echo ${PWMPORT} > /sys/class/pwm/${PWMCHIP}/export
+if [ -e /sys/class/pwm/${PWMCHIP}/export ]
+then
+	if [ ! -d /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT} ]
+	then
+		echo ${PWMPORT} > /sys/class/pwm/${PWMCHIP}/export
+	fi
+	if [ -d /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/ ]
+	then
+		echo $FREQ > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/period
+		echo $DUTYCYCLE > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/duty_cycle
 
-echo $FREQ > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/period
-echo $DUTYCYCLE > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/duty_cycle
-
-# enable it if it wasn't already
-echo 1 > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/enable
+		# enable it if it wasn't already
+		echo 1 > /sys/class/pwm/${PWMCHIP}/pwm${PWMPORT}/enable
+		exit 0
+	fi
+fi
+echo "pwm support not enabled, fan management disabled"
